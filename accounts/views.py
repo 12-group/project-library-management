812853d81ea8426fd5dca56636f5e9947c6880a6
dontTakeMyName <9.2ShinHayek@gmail.com>
2022-06-06 17:53:29 +0700

    account setting view

--- accounts/views.py
+++ accounts/views.py
@@ -50,7 +50,7 @@ def logoutUser(request):
     return redirect('home')
 
 def accountSettings(request):
-    reader = request.user.customer
+    reader = request.user.customer.reader
     form = ReaderForm(instance=reader)
 
     if request.method == 'POST':
