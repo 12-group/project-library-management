6772a8e1f5408603c121264eac906c6f9691db1d
Git4RJS <thanosbutnotdead@gmail.com>
2022-06-17 23:54:51 +0700

    heroku deploy

--- Library_management_system/settings.py
+++ Library_management_system/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p2ez95ap_i#@0$7!t@u=zttdd(@-oammlt9$5gekz&&z@+!cl%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['library-management-minh.herokuapp.com','127.0.0.1:8000']
 
 
 # Application definition
