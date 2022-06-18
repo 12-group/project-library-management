f524797567bea216eef845be574b86e936e9eebc
Nguyenduong2312 <57261677+Nguyenduong2312@users.noreply.github.com>
2022-06-18 09:31:32 +0700

    sửa allowed-host

--- Library_management_system/settings.py
+++ Library_management_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p2ez95ap_i#@0$7!t@u=zttdd(@-oammlt9$5gekz&&z@+!cl%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['library-management-minh.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
